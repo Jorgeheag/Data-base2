3eae41ed90fbb523662f14d36b70e44a510b56b2
jorgeheag <herreraajorgeh@gmail.com>
2022-06-29 22:05:33 -0500

    beta 1.1

--- utils/dataBase.js
+++ utils/dataBase.js
@@ -1,14 +1,17 @@
 const { Sequelize, DataTypes } = require('sequelize');
+const dotenv = require('dotenv')
+
+dotenv.config({path:'./config.env'})
 
 
 const db = new Sequelize({
     dialect:'postgres',
-    host:'localhost',
-    username:'postgres',
-    password:'jorgehh1989',
-    port: 5432,
-    database: 'Entregable 2',
-    logging: false
+    host: process.env.DB_HOST,
+    username:process.env.DB_USER,
+    password:process.env.DB_PASWWORD,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DB
+
 });
 
 module.exports = { db, DataTypes };
